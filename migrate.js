459ed49a10add09a724024327e9a6bfaa0d37a5b
Mr. Is <isaac.ardis@gmail.com>
2016-04-17 17:09:42 -0400

    implement walker

--- migrate.js
+++ migrate.js
@@ -4,20 +4,36 @@ var walk = require('walk')
 	, fs = require('fs')
 	, util = require('util')
 	, pg = require('./database.js')
+	, data = require('./data.js')
 	, walkMe
 	, walker
 	;
 
 
-walkMe = './testStorage';
-walker = walk.walk(walkMe); //, options
+walkMe = './testStorage/';
+walker = walk.walk(walkMe);
+
+function fileToJSON(body) {
+
+}
 
 walker.on("file", function (root, fileStats, next) {
-  fs.readFile(fileStats.name, function () {
-    // doStuff
-    console.log(util.inspect(fileStats));
-    next();
-  });
+  if (fileStats.name !== '.DS_Store' && fileStats.name.indexOf('.txt') < 0) {
+  	fs.readFile(walkMe + fileStats.name, 'utf8', function(err, body) {
+  		 
+  		if (err) { console.log('error! ' + err); return next(); }
+
+  		var j = JSON.parse(body);
+
+  		// TODO: pass to postgres here.
+  		console.log(util.inspect(j));
+
+
+  		next();
+  	});
+  } else {
+  	next();
+  }
 });
 
 walker.on("errors", function (root, nodeStatsArray, next) {
@@ -25,5 +41,5 @@ walker.on("errors", function (root, nodeStatsArray, next) {
 });
 
 walker.on("end", function () {
-  console.log("all done");
+  console.log("Finished walk.");
 });
